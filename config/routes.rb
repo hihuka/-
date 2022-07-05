84119f4eb7085b4aa6f3fa39c4dc015dd411b2ec
EC2 Default User <ec2-user@ip-172-31-16-117.ec2.internal>
2022-07-05 19:16:15 +0000

    課題2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'books/new'
   post "books" =>"books#create"
   get "books" =>"books#index"
-  get 'books/show'
   get 'books/edit'
   get '/top' => 'homes#top'
+  get 'books/:id' => 'books#show', as: "book"
 end
